9ac0b4077b5a06d511d3ac96d1320843091da76d
devsdmf <devsdmf@gmail.com>
2019-06-16 23:51:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# tPlayer
+
+tPlayer is a portable module player based on libmikmod that plays various formats of module audio files.
+
+More information soon.
+
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <mikmod.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        char *audio_file = argv[1];
+        MODULE *module;
+
+        MikMod_InitThreads();
+        MikMod_RegisterAllDrivers();
+        MikMod_RegisterAllLoaders();
+
+        md_mode |= DMODE_SOFT_MUSIC | DMODE_NOISEREDUCTION | DMODE_INTERP;
+        if (MikMod_Init("")) {
+            cout << "Could not initialize audio driver, reason: ", MikMod_strerror(MikMod_errno);
+            return 1;
+        }
+
+        module = Player_Load(audio_file, 64, 0);
+        if (module) {
+            Player_Start(module);
+
+            while (Player_Active()) {
+                usleep(10000);
+                MikMod_Update();
+            }
+
+            Player_Stop();
+            Player_Free(module);
+        } else {
+            cout << "Could not load module file, reason: ", MikMod_strerror(MikMod_errno);
+        }
+
+        MikMod_Exit();
+    }
+
+    return 0;
+}
+

4585ab42f5a03b034a8f5957ec2a97407eb25120
devsdmf <devsdmf@gmail.com>
2019-07-02 00:42:05 -0300

    Added base POC for curses

--- src/gui.cpp
+++ src/gui.cpp
@@ -10,12 +10,23 @@
 using namespace std;
 
 void start() {
-    //cout << "Hello!";
 
     initscr();
     printw("Hello World!\n");
     refresh();
+
+    int ch = getch();
+    if (ch == KEY_F(1)) {
+        printw("F1 key pressed");
+    } else {
+        printw("The pressed key is ");
+        attron(A_BOLD);
+        printw("%c", ch);
+        attroff(A_BOLD);
+    }
+    refresh();
     getch();
+
     endwin();
 }
 
